35fdcbbb81859cecbc12a775351d7c23be16cd0c
John <jonnyuz99@gmail.com>
2019-07-04 21:41:54 +0300

    no one will read it

---
+++ file_processing.c
@@ -0,0 +1,47 @@
+#include "differ.h"
+
+long long FSize(FILE * ptrfile)
+{
+//--------------------------------------------------------
+	if(ptrfile == NULL)
+	{
+		perror("Error of file reading");
+		return -1;
+	}
+//--------------------------------------------------------
+	long long curroff = ftell(ptrfile);
+
+	fseek(ptrfile, 0, SEEK_END);
+
+	long long size = ftell(ptrfile);
+
+	fseek(ptrfile, curroff, SEEK_SET);
+
+	return size;
+}
+
+char * MakeArray(FILE * fileptr)
+{
+	long long size = FSize(fileptr);
+
+	char * first_arr = (char *)calloc(size + 1, sizeof(char));
+	char * second_arr = (char *)calloc(size + 1, sizeof(char));
+
+	fread(first_arr, sizeof(char), size + 1, fileptr);//there can be not '\0' at the end
+
+	for(int i = 0; i < size; i++)
+		second_arr[i] = '\0';
+
+	for(int i = 0, j = 0; i < size; i++, j++)
+	{
+		if(first_arr[i] == '\n')
+			break;
+
+		if(first_arr[i] != ' ')
+			second_arr[j] = first_arr[i];
+		else
+			j--;
+	}
+
+	return second_arr;
+}
